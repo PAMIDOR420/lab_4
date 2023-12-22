bf52fcb454b9c2047351b585325dad360eedb430
PAMIDOR420 <keneksifire276@gmail.com>
2023-12-22 12:56:08 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Double []a={1.,5.,4.,3.,2.,1.};
+
+            MainFrame frame=new MainFrame();
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+    }
+}
